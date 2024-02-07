fd9bc9a25c18f739ea77218b287528b15346c626
Michael <20098628@mail.wit.ie>
2024-02-07 12:26:46 +0000

    getting started

---
+++ src/main/java/com/example/pillanalyser/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.pillanalyser;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class HelloController {
+    @FXML
+    private Button getStarted;
+
+    @FXML
+    protected void onButtonClick() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("menu.fxml"));
+            Parent root = loader.load();
+            Scene nextScene = new Scene(root);
+            Stage stage = (Stage) getStarted.getScene().getWindow();
+            stage.setScene(nextScene);
+            stage.show();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
