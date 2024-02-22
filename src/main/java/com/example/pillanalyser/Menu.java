6485b39427c36590c17d537d62caca9895d4d827
Michael <20098628@mail.wit.ie>
2024-02-22 12:25:42 +0000

    union find attempt 2

--- src/main/java/com/example/pillanalyser/Menu.java
+++ src/main/java/com/example/pillanalyser/Menu.java
@@ -1,25 +1,28 @@
 package com.example.pillanalyser;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Bounds;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.image.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.attribute.AclEntry;
+import java.util.*;
 
 //import static com.sun.tools.javac.resources.CompilerProperties.Fragments.Bound;
 
 public class Menu {
     @FXML
     private ImageView originalImage;
+
 
     @FXML
     private ImageView processedImage;
@@ -28,9 +31,11 @@ public class Menu {
 
     @FXML
     private Image originalPicture;
+    @FXML
+    private Slider thresholdSlider;
 
     private Map<String, Color> colorMap = new HashMap<>();
-    private HashMap<String,List<String>> allPillsPixels;
+    private HashMap<Integer,List<Integer>> allPillsPixels;
     int[] pixelArray;
 
     private UnionFind unionFind;
@@ -59,36 +64,47 @@ public class Menu {
 
     }
     public void initialize() {
-        enablePillSelection();
+
+        thresholdSlider.valueProperty().addListener((observable, oldValue, newValue) ->{
+            convertImageToBandW();
+        });
+
     }
     public void showImageDetails(File selectedFile) {
         if (selectedFile!= null) {
             originalImage.setImage(new Image(selectedFile.toURI().toString()));
         }
     }
-    public void convertImageToBandW(ActionEvent event) {
+    public void convertImageToBandW() {
         Image img = originalImage.getImage();
-        if (img!= null) {
+        if (img != null) {
             PixelReader pixelReader = img.getPixelReader();
             WritableImage bwImage = new WritableImage((int) img.getWidth(), (int) img.getHeight());
             PixelWriter pixelWriter = bwImage.getPixelWriter();
+            pixelArray = new int[(int) img.getWidth() * (int) img.getHeight()];
+            thresholdSlider.setMin(0);
+            thresholdSlider.setMax(1);
+            double thr = thresholdSlider.getValue();// Assuming thresholdSlider is defined elsewhere
             for (int y = 0; y < img.getHeight(); y++) {
                 for (int x = 0; x < img.getWidth(); x++) {
+                    int index = y * (int) img.getWidth() + x;
                     Color color = pixelReader.getColor(x, y);
-                    double brightness = color.getBrightness();
-                    if (brightness < 0.5) {
-                        pixelWriter.setColor(x, y, Color.BLACK);
-                    } else {
+                    if (color.getRed() < thr && color.getBlue() < thr && color.getGreen() < thr) {
                         pixelWriter.setColor(x, y, Color.WHITE);
+                        pixelArray[index] = index;
+                    } else {
+                        pixelWriter.setColor(x, y, Color.BLACK);
+                        pixelArray[index] = -1;
                     }
                 }
             }
             processedImage.setImage(bwImage);
         }
     }
-    private void enablePillSelection() {
-        originalImage.setOnMouseClicked(event -> {
-            Image img = originalImage.getImage();
+
+    public void enablePillSelection(MouseEvent event) {
+        Image img = originalImage.getImage();
+
             if (img != null) {
                 double mouseX = event.getX();
                 double mouseY = event.getY();
@@ -110,34 +126,38 @@ public class Menu {
 
 
             }
-        });
+
     }
 
 
     private void recordSelectedPill (Color pixelColor, double imageX, double imageY) {
-        String positionKey = String.format("%.2f,%.2f", imageX, imageY);
+
+//        String positionKey = String.format("%.2f,%.2f", imageX, imageY);
+        String positionKey = (int)imageX + "," + (int) imageY;
         colorMap.put(positionKey, pixelColor);
         unionFind.parent.put(positionKey, positionKey);
     }
     private void processPillSelection( double imageX, double imageY) {
-        String positionKey = String.format("%.2f,%.2f", imageX, imageY);
-        Color selectedColor = colorMap.get(positionKey);
-        for (int dx = -1; dx <= 1; dx++) {
-            for (int dy = -1; dy <= 1; dy++) {
-                if (dx == 0 && dy == 0) {
-                    continue;
-                }
-                String neighborPositionKey = String.format("%.2f,%.2f", imageX + dx, imageY + dy);
-                Color neighborColor = colorMap.get(neighborPositionKey);
-                if (neighborColor!= null &&!neighborColor.equals(selectedColor)) {
-                    unionFind.union(positionKey, neighborPositionKey);
-                }
-            }
-        }
+
+//        String positionKey = String.format("%.2f,%.2f", imageX, imageY);
+//        Color selectedColor = colorMap.get(positionKey);
+//        for (int dx = -1; dx <= 1; dx++) {
+//            for (int dy = -1; dy <= 1; dy++) {
+//                if (dx == 0 && dy == 0) {
+//                    continue;
+//                }
+////                String neighborPositionKey = String.format("%.2f,%.2f", imageX + dx, imageY + dy);
+//                Color neighborColor = colorMap.get(neighborPositionKey);
+//                if (neighborColor!= null &&!neighborColor.equals(selectedColor)) {
+//                    unionFind.union(positionKey, neighborPositionKey);
+//                }
+//            }
+//        }
     }
 //    List<Integer> disjointSets= new ArrayList<>();
 
     public void selectPill(Rectangle r) {
+
         // Get the dimensions and position of the rectangle
         double rectangleX = r.getX();
         double rectangleY = r.getY();
@@ -151,24 +171,32 @@ public class Menu {
         // Iterate through the pixels within the bounding box of the rectangle
         for (double y = rectangleY; y < rectangleY + rectangleHeight; y++) {
             for (double x = rectangleX; x < rectangleX + rectangleWidth; x++) {
-                // Convert coordinates to pixel index
                 int index = (int) (y * imageWidth + x);
-
-                String positionKey = String.format("%.2f,%.2f", x, y );
-
-                // Find the root of the pixel
-                String root = unionFind.find(positionKey);
+                for (int i = index; i < pixelArray.length; i++) {
+                    // Convert coordinates to pixel index
 
 
-                // Get the color associated with the root and set it to the corresponding pixel in the new image
-                Color color = colorMap.get(root);
-                if (color != null) {
-                    writableImage.getPixelWriter().setColor((int) x, (int) y, color);
+
+                    // Find the root of the pixel
+                    int root = unionFind.find(pixelArray,index);
+                    if (root != -1 && i==unionFind.find(pixelArray,index)) {}
+                          {
+                              // Get the color associated with the root and set it to the corresponding pixel in the new image
+                              Color color = colorMap.get(root);
+                              if (color != null) {
+                                  writableImage.getPixelWriter().setColor((int) x, (int) y, color);
+                              }
+                              else{
+                                  index++;
+                              }
+                          }
+
                 }
             }
         }
 
         originalImage.setImage(writableImage);
+
     }
     public void displayPillsSelected() {
         if (colorMap != null) {
@@ -178,31 +206,60 @@ public class Menu {
 
             // Iterate over the pixels and identify the pills
             this.allPillsPixels = new HashMap<>();
+            double top = 0;
+            double bottom = 0;
+            double left = 0;
+            double right = 0;
             int imageWidth = (int) originalImage.getFitWidth();
             int imageHeight = (int) originalImage.getFitHeight();
+            Bounds bounds = originalImage.getLayoutBounds();
+            double scaleX = bounds.getWidth() / originalImage.getImage().getWidth();
+            double scaleY = bounds.getHeight() / originalImage.getImage().getHeight();
 
-            for (int y = 0; y < imageHeight; y++) {
-                for (int x = 0; x < imageWidth; x++) {
 
-
-                    String positionKey = String.format("%d,%d", (int) x, (int) y);
-                    String root = unionFind.find(positionKey);
-
-                    List<String> pixels = allPillsPixels.getOrDefault(root, new ArrayList<>());
-                    pixels.add(positionKey);
-                    allPillsPixels.put(root, pixels);
-                    selectPill(createRectangleAroundPill(x, y));
+            for (int i = 0; i < pixelArray.length; i++) {
+                int root = unionFind.find(pixelArray, i);
+                if (root != -1) {
+                    if (!allPillsPixels.containsKey(root)) {
+                        allPillsPixels.put(root, new ArrayList<>());
+                    }
+                    allPillsPixels.get(root).add(i);
                 }
+            }
 
+            for (int root : allPillsPixels.keySet()) {
+                // The line `top= Collections.min(allPillsPixels.get(root))/(int)originalPicture.getWidth();` is
+                // attempting to calculate the top position of a pill based on the minimum pixel value in the list
+                // of pixels associated with a particular pill root, divided by the width of the original picture.
+                List<Integer> pixels = allPillsPixels.get(root);
+                top = Collections.min(allPillsPixels.get(root)) / (int) originalPicture.getWidth();
+                bottom = Collections.max(allPillsPixels.get(root)) / (int) originalPicture.getWidth();
+                left = Collections.min(allPillsPixels.get(root), (a, b) -> a % imageWidth - b % imageWidth) % imageWidth;//negative if a should come before b and pos if a comes after, zero if theyre the same
+                right = Collections.max(allPillsPixels.get(root), (a, b) -> a % imageWidth - b % imageWidth) % imageWidth;
+                if (root != -1) {
+                    top *= scaleX;
+                    right *= scaleY;
+                    bottom *= scaleX;
+                    left *= scaleY;
+                    double centerX = (left + right) / 2;
+                    double centerY = (top + bottom) / 2;
+
+
+                    Rectangle rectangle = new Rectangle(centerX, centerY, 1, 1);
+
+                    rectangle.onMouseClickedProperty().set(e -> selectPill(rectangle));
+                    System.out.println("here4");
+                }
             }
         }
     }
 
-    private Rectangle createRectangleAroundPill(double x, double y) {
-        double relativeX = x * (originalImage.getFitWidth() / originalPicture.getWidth());
-        double relativeY = y * (originalImage.getFitHeight() / originalPicture.getHeight());
 
-        Rectangle rectangle = new Rectangle(relativeX, relativeY, 1, 1);
+
+    private Rectangle createRectangleAroundPill(double x, double y) {
+
+
+        Rectangle rectangle = new Rectangle(x, y, 1, 1);
         rectangle.setFill(Color.TRANSPARENT);
         rectangle.setStroke(Color.RED);
         rectangle.setStrokeWidth(2);
