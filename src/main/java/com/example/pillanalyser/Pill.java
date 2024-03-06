3f78489d233cc5353cd01a93627914cffb2084c4
Michael <20098628@mail.wit.ie>
2024-03-06 15:10:36 +0000

    all working can now pick colours before performing hsb

---
+++ src/main/java/com/example/pillanalyser/Pill.java
@@ -0,0 +1,16 @@
+package com.example.pillanalyser;
+
+import javafx.scene.paint.Color;
+
+public class Pill {
+    private Color color;
+
+    public Pill(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+}
